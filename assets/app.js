b7b50941ea65566d01e074c760513afbe9469f5b
nafibal <baliya363@gmail.com>
2021-06-26 22:07:41 +0700

    fixed slider

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,16 @@
-const prevArrowBtn = document.querySelector(".slick-prev");
-const nextArrowBtn = document.querySelector(".slick-next");
-
-console.log(prevArrowBtn);
+const dotContainer = document.querySelector(".dot-container");
 
 $(".slider").slick({
   accessibility: true,
   adaptiveHeight: true,
+  arrows: true,
+  dots: true,
+  appendDots: dotContainer,
+  appendArrows: dotContainer,
   prevArrow:
     '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
   nextArrow:
     '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
-  arrows: true,
-  dots: true,
   infinite: true,
   mobileFirst: true,
 });
